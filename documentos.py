afb2d9ab36a89aac5ef3b26072fb4a287faa316d
Federico <federicolami@hotmail.com>
2026-05-11 12:56:01 -0300

    elimino dependencias externas de embeddings, usa chromadb nativo

--- documentos.py
+++ documentos.py
@@ -1,7 +1,5 @@
 import chromadb
-from sentence_transformers import SentenceTransformer
 import fitz
-
 
 def cargar_documento(ruta):
     if (ruta.endswith(".pdf")):
@@ -11,11 +9,11 @@ def cargar_documento(ruta):
             texto += pagina.get_text()
         contenido = texto
     else:
-        with open(ruta, "r", encoding = "utf-8") as text:
+        with open(ruta, "r", encoding="utf-8") as text:
             contenido = text.read()
     return contenido
 
-def dividir_en_fragmentos(texto, size = 500, solapamiento = 50):
+def dividir_en_fragmentos(texto, size=500, solapamiento=50):
     i = 0
     listaTexto = []
     while (i < len(texto)):
@@ -23,19 +21,11 @@ def dividir_en_fragmentos(texto, size = 500, solapamiento = 50):
         i += size - solapamiento
     return listaTexto
 
-def guardar_en_chromadb(fragmentos,nombre_coleccion):
+def guardar_en_chromadb(fragmentos, nombre_coleccion):
     cliente = chromadb.Client()
-    
-    modelo = SentenceTransformer("all-MiniLM-L6-v2")
-    embeddings = modelo.encode(fragmentos)
-
     coleccion = cliente.get_or_create_collection(nombre_coleccion)
-    ids = []
-    for i in range(len(fragmentos)):
-        ids.append(f"fragmento_{i}")
+    ids = [f"fragmento_{i}" for i in range(len(fragmentos))]
     coleccion.add(
-        documents = fragmentos,
-        embeddings = embeddings,
-        ids = ids
+        documents=fragmentos,
+        ids=ids
     )
-    
